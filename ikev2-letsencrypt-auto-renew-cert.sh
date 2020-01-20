f5e9d49b051cec507e9defa5227a79e861046447
will <trepwq@gmail.com>
2020-01-21 04:03:05 +0800

    Create ikev2-letsencrypt-auto-renew-cert.sh

---
+++ ikev2-letsencrypt-auto-renew-cert.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+CERT_DOMAIN=vpn.example.com
+CLOUDFLARE_CREDENTIALS='/root/letsencrypt/cloudflarednsapi.conf'
+LE_EMAIL='example@gmail.com'
+
+
+function checkcertbot(){
+type certbot
+if [ $? -eq 0 ];
+then
+pip install certbot-dns-cloudflare
+fi
+}
+
+function checkcert(){
+if [ ! -f "/etc/letsencrypt/live/${CERT_DOMAIN}/cert.pem" ];
+then renewipseccert
+else
+CERT_EXPIRE=$(openssl x509 -checkend 1728000 -noout -in /etc/letsencrypt/live/$CERT_DOMAIN/cert.pem|grep not)
+if [ -z "$CERT_EXPIRE" ];
+then
+renewipseccert
+else
+echo "The cert will not expire in 20 days,nothing to do."
+fi
+fi
+}
+
+
+function renewipseccert(){
+/usr/local/bin/certbot  certonly -d ${CERT_DOMAIN} \
+--agree-tos \
+--non-interactive \
+--email $LE_EMAIL  \
+--dns-cloudflare \
+--dns-cloudflare-credentials ${CLOUDFLARE_CREDENTIALS}
+cp /etc/letsencrypt/live/$CERT_DOMAIN/chain.pem /usr/local/etc/ipsec.d/cacerts/ca.cert.pem
+cp /etc/letsencrypt/live/$CERT_DOMAIN/cert.pem /usr/local/etc/ipsec.d/certs/server.cert.pem
+cp /etc/letsencrypt/live/$CERT_DOMAIN/privkey.pem /usr/local/etc/ipsec.d/private/server.pem
+service ipsec restart
+fi
+}
+
+checkcertbot
+checkcert
